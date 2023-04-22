654760cdfeeb6576aa2d8209428ad65c2cd9036b
Dayanne Santos <69702472+dayannesantos@users.noreply.github.com>
2023-04-22 09:33:40 -0300

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,16 @@
+let letter = prompt("Digite uma letra:");
+
+switch (letter) {
+  case "a":
+    alert(`Você digitou a letra ${letter}`);
+    break;
+  case "b":
+    alert(`Você digitou a letra ${letter}`);
+    break;
+  case "c":
+    alert(`Você digitou a letra ${letter}`);
+    break;
+  case "":
+    alert("Você não digitou nenhuma letra");
+    break;
+}
